9357774eca6490393939e04743586b9bd55bee7f
nadav ben shimon <nadavnbs@gmail.com>
2018-03-19 15:25:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object.key = value
+  return object
+}
+
