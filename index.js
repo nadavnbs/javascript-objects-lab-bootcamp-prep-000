2410ee58d1e93539de67dad1b74a8a8dd8b83ec3
nadav ben shimon <nadavnbs@gmail.com>
2018-03-19 16:26:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 function deleteFromObjectByKey(object, key){
   var newRecipes = Object.assign({}, object)
-  delete newRecipes.key
+  return delete newRecipes.key
+  
 }
 
 
