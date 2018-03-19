25111d40d646e84fdaa83cbca6c8fb01d2e784c4
nadav ben shimon <nadavnbs@gmail.com>
2018-03-19 16:00:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var recipes = {prop:1}
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assing(object,{key:value})
+  Object.assing(object,{key:value})
+  return object
 }
 
