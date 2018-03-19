45b5b5909b3abefc6f2513c9d65cfe1a59c80f62
nadav ben shimon <nadavnbs@gmail.com>
2018-03-19 16:06:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ var recipes = {prop:1}
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({},object,{[key]:value})
 }
-
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
